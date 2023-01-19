c745ed008f648c4732fcec2642e7198474fe34af
onoshkinivan <98639022+onoshkinivan@users.noreply.github.com>
2023-01-19 19:50:09 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -14,30 +14,30 @@ function start(cells) {
                 console.log(`Победитель ${this.textContent}`)
             }
         })
-    }
-}
-
-function iswinner(cells) {
-    let combs = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ]
-
-    for (let comb of combs) {
-        if (cells(comb[0]).textContent == cells(comb[1]).textContent &&
-            cells(comb[1]).textContent == cells(comb[2]).textContent &&
-            cells(comb[0]).textContent != '') {
-            return true;
-        } else {
-            return false;
+        function iswinner(cells) {
+            let combs = [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8],
+                [0, 4, 8],
+                [2, 4, 6]
+            ]
+        
+            for (let comb of combs) {
+                if (cells(comb[0]).textContent == cells(comb[1]).textContent &&
+                    cells(comb[1]).textContent == cells(comb[2]).textContent &&
+                    cells(comb[0]).textContent != '') {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }
     }
 }
+
 
 start(cells);

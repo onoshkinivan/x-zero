b44665d6fd99b0e6e3b562f53d5250c5f22644ea
onoshkinivan <98639022+onoshkinivan@users.noreply.github.com>
2023-01-21 15:50:02 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,43 +1,52 @@
 let cells = document.querySelectorAll('#field td');
-let i = 2;
+let result = document.querySelector('#winner')
+
 function start(cells) {
+    let i = 2;
     for (let cell1 of cells) {
         cell1.addEventListener('click', function stat() {
             if (i % 2 == 0) {
-                this.textContent = 'X';
+                this.textContent = '☭';
+                this.style.color = 'red'
             } else {
-                this.textContent = '0';
+                this.textContent = '☮';
+                this.style.color = 'white'
             }
-            i++;
             this.removeEventListener('click', stat);
             if (iswinner(cells) === true) {
-                console.log(`Победитель ${this.textContent}`)
+                result.style.height = '150px';
+                result.textContent = `Победитель ${this.textContent}`;
+            } else if (i == 10) {
+                result.style.height = '150px';
+                result.textContent = `Ничья!`;
             }
+            i++
         })
-        function iswinner(cells) {
-            let combs = [
-                [0, 1, 2],
-                [3, 4, 5],
-                [6, 7, 8],
-                [0, 3, 6],
-                [1, 4, 7],
-                [2, 5, 8],
-                [0, 4, 8],
-                [2, 4, 6]
-            ]
-        
-            for (let comb of combs) {
-                if (cells(comb[0]).textContent == cells(comb[1]).textContent &&
-                    cells(comb[1]).textContent == cells(comb[2]).textContent &&
-                    cells(comb[0]).textContent != '') {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        }
     }
 }
 
+function iswinner(cells) {
+    let combs = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    for (let comb of combs) {
+        if (cells[comb[0]].textContent == cells[comb[1]].textContent &&
+            cells[comb[1]].textContent == cells[comb[2]].textContent &&
+            cells[comb[0]].textContent != '') {
+            return true;
+        }
+    }
+    return false;
+}
 
 start(cells);
+
+
